ca81180561ede681f4da38380be64fb62a2c9a2c
Tan Duc <ductan10092003@gmail.com>
2022-09-24 23:45:14 +0700

    Update KeyEvent

--- src/uet/oop/bomberman/BombermanGame.java
+++ src/uet/oop/bomberman/BombermanGame.java
@@ -7,6 +7,7 @@ import javafx.scene.Scene;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
 import javafx.stage.Stage;
+import uet.oop.bomberman.controlSystem.KeyListener;
 import uet.oop.bomberman.entities.Bomber;
 import uet.oop.bomberman.entities.Entity;
 import uet.oop.bomberman.entities.Grass;
@@ -25,6 +26,7 @@ public class BombermanGame extends Application {
     private Canvas canvas;
     private List<Entity> entities = new ArrayList<>();
     private List<Entity> stillObjects = new ArrayList<>();
+    private KeyListener keyEvent;
     public Map map = new Map();
 
 
@@ -44,6 +46,7 @@ public class BombermanGame extends Application {
 
         // Tao scene
         Scene scene = new Scene(root);
+        keyEvent = new KeyListener(scene);
 
         // Them scene vao stage
         stage.setScene(scene);
@@ -60,24 +63,24 @@ public class BombermanGame extends Application {
 
         map.createMap(1);
 
-        Entity bomberman = new Bomber(1, 1, Sprite.player_right.getFxImage());
+        Entity bomberman = new Bomber(1, 1, Sprite.player_right.getFxImage(), keyEvent);
         entities.add(bomberman);
     }
 
-    public void createMap() {
-        for (int i = 0; i < WIDTH; i++) {
-            for (int j = 0; j < HEIGHT; j++) {
-                Entity object;
-                if (j == 0 || j == HEIGHT - 1 || i == 0 || i == WIDTH - 1) {
-                    object = new Wall(i, j, Sprite.wall.getFxImage());
-                }
-                else {
-                    object = new Grass(i, j, Sprite.grass.getFxImage());
-                }
-                stillObjects.add(object);
-            }
-        }
-    }
+//    public void createMap() {
+//        for (int i = 0; i < WIDTH; i++) {
+//            for (int j = 0; j < HEIGHT; j++) {
+//                Entity object;
+//                if (j == 0 || j == HEIGHT - 1 || i == 0 || i == WIDTH - 1) {
+//                    object = new Wall(i, j, Sprite.wall.getFxImage());
+//                }
+//                else {
+//                    object = new Grass(i, j, Sprite.grass.getFxImage());
+//                }
+//                stillObjects.add(object);
+//            }
+//        }
+//    }
 
     public void update() {
         entities.forEach(Entity::update);
@@ -90,6 +93,5 @@ public class BombermanGame extends Application {
         }
         entities.forEach(g -> g.render(gc));
     }
-    //tesst
 
 }
